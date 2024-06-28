0ec0ac83933d0de1eb2b7d6b88ec3c5c615067d2
leandromacedosilva <iamfulldevelope@gmail.com>
2024-06-28 11:30:39 -0300

    commited update in project developer soccer club with class Player.java

--- src/com/developersoccerclub/model/Player.java
+++ src/com/developersoccerclub/model/Player.java
@@ -5,5 +5,10 @@ package com.developersoccerclub.model;
  * @author leandro
  */
 public class Player {
-    
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String posicao;
+    private float salario;
 }
