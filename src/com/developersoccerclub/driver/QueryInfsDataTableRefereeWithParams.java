0c2fe6755aa70437cf4a0c636207acf767442805
leandromacedosilva <iamfulldevelope@gmail.com>
2024-07-14 21:52:40 -0300

    Update project DeveloperSoccerClub exercice JPA in repository

--- src/com/developersoccerclub/driver/QueryInfsDataTableRefereeWithParams.java
+++ src/com/developersoccerclub/driver/QueryInfsDataTableRefereeWithParams.java
@@ -15,9 +15,10 @@ public class QueryInfsDataTableRefereeWithParams {
         EntityManager em = JpaUtil.openConnection();
         em.getTransaction().begin();
         
-        String query = "select r.nome,r.sobrenome,r.cpf,a.rua,a.cidade,a.bairro from Referee r INNER JOIN Address a ON r.id = :id";
+        //String query = "select r.nome,r.sobrenome,r.cpf,a.rua,a.cidade,a.bairro from Referee r INNER JOIN Address a ON r.id = :id";
+        String query = "select r from Referee r WHERE r.id = :id";
                 Query queryh = em.createQuery(query);
-                queryh.setParameter("id", 51);
+                queryh.setParameter("id", 1);
                 List<Referee> referees = queryh.getResultList();
                 System.out.println("RESULTADO" + referees);
         em.getTransaction().commit();
