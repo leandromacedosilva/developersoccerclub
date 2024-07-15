ba4d731dc8023b8c7df695a675401d3371ae1253
leandromacedosilva <iamfulldeveloper@gmail.com>
2024-07-15 16:56:11 -0300

    Merge origin/master
    # Conflicts:
    #	nbproject/build-impl.xml
    #	nbproject/genfiles.properties
    #	nbproject/project.properties
    #	nbproject/project.xml

---
+++ src/com/developersoccerclub/model/util/Message.java
@@ -0,0 +1,14 @@
+package com.developersoccerclub.model.util;
+
+/**
+ *
+ * @author itccolina
+ */
+public class Message {
+    
+    public String msg;
+    
+    public String message(String message){
+        return msg = message;
+}
+}
